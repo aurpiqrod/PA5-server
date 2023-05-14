a4232f356a63e631eb33ac226f379b83315f0c1e
Aureliano Piqueras Rodríguez <aurpiqrod@alum.us.es>
2023-05-15 01:40:27 +0200

    Firma

--- src/main/java/com/insegus/pai5/App.java
+++ src/main/java/com/insegus/pai5/App.java
@@ -19,16 +19,19 @@ import java.security.Signature;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Base64;
+import java.util.Properties;
 
 import javax.net.ssl.*;
 
 public class App {
-    private static boolean verifySignature(String data, String signature) {
+    
+    private static boolean verifySignature(String data, String signature, String clientCertPath) {
         try {
             // Cargar el certificado público del cliente desde el archivo del certificado
             CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-            FileInputStream certificateFile = new FileInputStream(App.class.getResource("/keystore/client_certificate.crt").getPath());
+            FileInputStream certificateFile = new FileInputStream(App.class.getResource(clientCertPath).getPath());
             X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(certificateFile);
+            //System.out.println("Certificado del cliente: " + certificate.toString());
     
             // Obtener la clave pública del certificado
             PublicKey publicKey = certificate.getPublicKey();
@@ -56,97 +59,112 @@ public class App {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         try {
-            // Obtener la ruta del archivo del keystore y su contraseña desde las
-            // propiedades del sistema
-            String keystorePassword = "password";
-            String keystorePath = App.class.getResource("/keystore/keystore.jks").getPath();
+            // Cargar las propiedades desde el archivo de configuración
+            Properties properties = new Properties();
+            FileInputStream configFile = new FileInputStream(App.class.getResource("/config/config.properties").getPath());
+            properties.load(new InputStreamReader(configFile));
 
-            // Cargar el keystore desde la ruta y la contraseña proporcionadas
+            // Obtener las contraseñas y rutas de los recursos desde las propiedades cargadas
+            String keystorePassword = properties.getProperty("keystorePassword");
+            String truststorePassword = properties.getProperty("truststorePassword");
+            String keystorePath = App.class.getResource(properties.getProperty("keystorePath")).getPath();
+            String truststorePath = App.class.getResource(properties.getProperty("truststorePath")).getPath();
+            String clientCertPath = properties.getProperty("clientCertPath");
+
+            // Cargar el keystore y truststore desde las rutas y contraseñas proporcionadas
             KeyStore keystore = KeyStore.getInstance("JKS");
             FileInputStream keystoreFile = new FileInputStream(keystorePath);
             keystore.load(keystoreFile, keystorePassword.toCharArray());
 
-            // Crear un SSLContext y un SSLServerSocketFactory a partir del keystore cargado
-            SSLContext sslContext = SSLContext.getInstance("TLS");
+            KeyStore truststore = KeyStore.getInstance("JKS");
+            FileInputStream truststoreFile = new FileInputStream(truststorePath);
+            truststore.load(truststoreFile, truststorePassword.toCharArray());
+
+            // Crear un SSLContext y un SSLServerSocketFactory a partir del keystore y truststore cargados
             KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
             kmf.init(keystore, keystorePassword.toCharArray());
-            sslContext.init(kmf.getKeyManagers(), null, null);
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(truststore);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
             SSLServerSocketFactory factory = sslContext.getServerSocketFactory();
 
             // Crear un SSLServerSocket en el puerto 7070
             SSLServerSocket serverSocket = (SSLServerSocket) factory.createServerSocket(7070);
 
-            // Configurar parámetros de seguridad SSL/TLS en el servidor SSL
-            SSLParameters sslParams = new SSLParameters();
-            String[] protocols = new String[] { "TLSv1.3" };
-            String[] cipherSuites = new String[] {
-                    "TLS_AES_128_GCM_SHA256",
-                    "TLS_AES_256_GCM_SHA384",
-                    "TLS_CHACHA20_POLY1305_SHA256"
-            };
-            sslParams.setProtocols(protocols);
-            sslParams.setCipherSuites(cipherSuites);
-            serverSocket.setSSLParameters(sslParams);
-
-            while (true) {
-                System.err.println("Waiting for connection...");
-
-                // Esperar y aceptar conexiones de clientes
-                SSLSocket socket = (SSLSocket) serverSocket.accept();
-
-                // Abrir BufferedReader para leer datos del cliente
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                // Leer la petición completa del cliente
-                String clientRequest = input.readLine();
-                System.out.println("Petición recibida: " + clientRequest);
-
-                // Parsear la petición del cliente como un objeto JSON
-                JSONObject requestData = new JSONObject(clientRequest);
-
-                // Extraer y mostrar los datos del objeto JSON
-                System.out.println("Datos del cliente: " + requestData.toString(2));
-
-                // Obtener la firma de la solicitud
-                String signature = requestData.optString("signature");
-
-                if (signature != null) {
-                    // Verificar la firma utilizando el certificado público del cliente
-                    boolean isSignatureValid = verifySignature(requestData.toString(), signature);
-
-                    if (isSignatureValid) {
-                        // La firma es válida, proceder con el procesamiento de la solicitud
-                        // ...
-                        // Resto del código de procesamiento de la solicitud
-                    } else {
-                        // La firma no es válida, rechazar la solicitud
-                        System.out.println("La firma de la solicitud no es válida. La solicitud será rechazada.");
-                    }
-                } else {
-                    // No se proporcionó una firma, rechazar la solicitud
-                    System.out.println("La solicitud no está firmada. La solicitud será rechazada.");
-                }
-
-                // Extraer y mostrar los datos del objeto JSON
-                System.out.println("Datos del cliente: " + requestData.toString(2));
-
-                // Abrir PrintWriter para enviar datos al cliente
-                PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-
-                // Enviar una respuesta al cliente
-                output.println("Petición recibida y procesada.");
-                output.flush(); // Asegúrate de que la respuesta se envíe antes de cerrar los recursos
-
-                // Cerrar recursos
-                output.close();
-                input.close();
-                socket.close();
-            }
-
-        } catch (IOException | KeyStoreException | NoSuchAlgorithmException
-                | CertificateException | UnrecoverableKeyException | KeyManagementException ex) {
-            System.err.println("Error: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-    }
-}
+              // Configurar parámetros de seguridad SSL/TLS en el servidor SSL
+              SSLParameters sslParams = new SSLParameters();
+              String[] protocols = new String[] { "TLSv1.3" };
+              String[] cipherSuites = new String[] {
+                      "TLS_AES_128_GCM_SHA256",
+                      "TLS_AES_256_GCM_SHA384",
+                      "TLS_CHACHA20_POLY1305_SHA256"
+              };
+              sslParams.setProtocols(protocols);
+              sslParams.setCipherSuites(cipherSuites);
+              serverSocket.setSSLParameters(sslParams);
+  
+              while (true) {
+                  System.err.println("Waiting for connection...");
+  
+                  // Esperar y aceptar conexiones de clientes
+                  SSLSocket socket = (SSLSocket) serverSocket.accept();
+  
+                  // Abrir BufferedReader para leer datos del cliente
+                  BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+  
+                  // Leer la petición completa del cliente
+                  String clientRequest = input.readLine();
+                  System.out.println("Petición recibida: " + clientRequest);
+  
+                  // Parsear la petición del cliente como un objeto JSON
+                  JSONObject requestData = new JSONObject(clientRequest);
+  
+                  // Extraer y mostrar los datos del objeto JSON
+                  //System.out.println("Datos del cliente: " + requestData.toString(2));
+  
+                  // Obtener la firma de la solicitud
+                  String signature = requestData.optString("signature");
+  
+                  if (signature != null) {
+                      // Verificar la firma utilizando el certificado público del cliente
+                      requestData.remove("signature");
+                      boolean isSignatureValid = verifySignature(requestData.toString(), signature, clientCertPath);
+  
+                      if (isSignatureValid) {
+                          // La firma es válida, proceder con el procesamiento de la solicitud
+                          System.out.println("La firma de la solicitud es válida. La solicitud será guardada.");
+                      } else {
+                          // La firma no es válida, rechazar la solicitud
+                          System.out.println("La firma de la solicitud no es válida. La solicitud será rechazada.");
+                      }
+                  } else {
+                      // No se proporcionó una firma, rechazar la solicitud
+                      System.out.println("La solicitud no está firmada. La solicitud será rechazada.");
+                  }
+  
+                  // Extraer y mostrar los datos del objeto JSON
+                  System.out.println("Datos del cliente: " + requestData.toString(2));
+  
+                  // Abrir PrintWriter para enviar datos al cliente
+                  PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+  
+                  // Enviar una respuesta al cliente
+                  output.println("Petición recibida y procesada.");
+                  output.flush(); // Asegúrate de que la respuesta se envíe antes de cerrar los recursos
+  
+                  // Cerrar recursos
+                  output.close();
+                  input.close();
+                  socket.close();
+              }
+  
+          } catch (IOException | KeyStoreException | NoSuchAlgorithmException
+                  | CertificateException | UnrecoverableKeyException | KeyManagementException ex) {
+              System.err.println("Error: " + ex.getMessage());
+              ex.printStackTrace();
+          }
+      }
+  }
